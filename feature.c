dc690baeca347a4b9a3f8f6b276f1fb61693fbd5
PiGreco2312 <82760976+PiGreco2312@users.noreply.github.com>
2026-09-07 16:16:45 +0200

    added number_uses

--- feature.c
+++ feature.c
@@ -10,17 +10,19 @@ FEATURE NEW_feature(void){
         strcpy(f->desc, "");
         f->action = ACTION;
         f->saving_throw = 0;
+        f->number_uses=0;
     }
     return f;
 }
 
-FEATURE ADD_feature(char *n, char *d, int st, action_type a){
+FEATURE ADD_feature(char *n, char *d, int st, int uses, action_type a){
     FEATURE f=(FEATURE)malloc(sizeof(struct feature));
     if(f!=NULL) {
         strncpy(f->name, n, MAX_NAME-1);
         strncpy(f->desc, d, MAX_DESC-1);
         f->saving_throw = st;
         f->action = a;
+        f->number_uses = uses;
     }
     return f;
 }
